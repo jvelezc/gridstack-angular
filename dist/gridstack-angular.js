eca4aa4dcfeeb703dbdb916e342771bb0229f66c
Jose <goodealsnow@gmail.com>
2016-07-24 17:46:05 -0700

    Update gridstack-angular.js

--- dist/gridstack-angular.js
+++ dist/gridstack-angular.js
@@ -1,6 +1,6 @@
 /**
  * gridstack-angular - Angular Gridstack.js directive
- * @version v0.4.0
+ * @version v1.0.0
  * @author Kevin Dietrich
  * @link https://github.com/kdietrich/gridstack-angular#readme
  * @license MIT
